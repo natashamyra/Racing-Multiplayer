107f1fc81a1bf93bded98e53a622e45b736bc77b
nuramyra <nuramyra.ntsh@gmail.com>
2021-10-18 22:24:00 +0800

    PhotonPun2
    - Create Room
    - Join Room
    - Quick start room

---
+++ Assets/Script/Mechanic/ConnectedtoServer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+
+public class ConnectedtoServer : MonoBehaviourPunCallbacks
+{
+    //private void Start()
+    //{
+    //    PhotonNetwork.ConnectUsingSettings();
+    //}
+
+    //public override void OnConnectedToMaster()
+    //{
+    //    PhotonNetwork.JoinLobby();
+    //}
+
+    //public override void OnJoinedLobby()
+    //{
+    //    SceneManager.LoadScene("SampleScene");
+    //}
+    //public override void OnConnectedToMaster()
+    //{
+    //    SceneManager.LoadScene(0);
+    //}
+
+
+}
